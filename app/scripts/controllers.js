de80343fe52335f5a9633891d0607166875d25af
Shyam Seshadri <shyamseshadri@gmail.com>
2013-12-02 07:18:49 +0530

    Session 1 - Step 3: Controllers, ng-repeat and other directives

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,4 +1,27 @@
 angular.module('stockMarketApp').controller('AppCtrl', [function() {
   var self = this;
-  self.welcomeMessage = 'Hello from Shyam';
+
+  self.stocks = [{
+    "ticker": "TWTR",
+    "name": "Twitter Inc",
+    "price": 23,
+    "previous": 21,
+    "size": "Medium"
+  }, {
+    "ticker": "GOOG",
+    "name": "Google Inc",
+    "price": 884,
+    "previous": 899,
+    "size": "Large"
+  }];
+
+  self.getChange = function(stock) {
+    return Math.ceil(((stock.price - stock.previous) / stock.previous) * 100);
+  };
+  self.getChangeClass = function(stock) {
+    return {
+      positive: stock.price > stock.previous,
+      negative: stock.price <= stock.previous
+    }
+  };
 }]);

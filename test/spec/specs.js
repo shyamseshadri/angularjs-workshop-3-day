2daf33efb2af3eec7828877831b6409a429804c4
Shyam Seshadri <shyamseshadri@gmail.com>
2013-12-03 23:17:54 +0530

    Session 4 - Step 3: test for registration flow

--- test/spec/specs.js
+++ test/spec/specs.js
@@ -42,3 +42,34 @@ describe('Controller: LandingCtrl', function () {
     expect(ctrl.stocks).toEqual([{ticker: 'GOOG'}, {ticker: 'AMZN'}]);
   });
 });
+
+describe('Controller: AuthCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('stockMarketApp'));
+
+  var ctrl, mockBackend, $loc, userService;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $httpBackend, $location, UserService) {
+    mockBackend = $httpBackend;
+    userService = UserService;
+    $loc = $location;
+
+    ctrl = $controller('AuthCtrl');
+  }));
+
+  it('should register the user', function () {
+    ctrl.username = 'shyam';
+    ctrl.password = 'test';
+
+    mockBackend.expectPOST('/api/register', {username: 'shyam', password: 'test'}).respond({user: {name: 'shyam'}});
+
+    expect(userService.isLoggedIn()).toBeFalsy();
+    ctrl.register();
+
+    mockBackend.flush();
+    expect($loc.path()).toEqual('/mine');
+    expect(userService.isLoggedIn()).toBeTruthy();
+  });
+});

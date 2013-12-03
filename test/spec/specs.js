0c5766f84b6c9ca3e3586b369f9e6628351cdcec
Shyam Seshadri <shyamseshadri@gmail.com>
2013-12-03 23:16:26 +0530

    Session 4 - Step 2: Add test for LandingCtrl with mock server

--- test/spec/specs.js
+++ test/spec/specs.js
@@ -19,3 +19,26 @@ describe('Service: AlertService', function () {
   });
 
 });
+
+describe('Controller: LandingCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('stockMarketApp'));
+
+  var ctrl, mockBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $httpBackend) {
+    mockBackend = $httpBackend;
+    mockBackend.expectGET('/api/stocks').respond([{ticker: 'GOOG'}, {ticker: 'AMZN'}]);
+    ctrl = $controller('LandingCtrl');
+  }));
+
+  it('should fetch all stocks on load', function () {
+    expect(ctrl.stocks).toEqual([]);
+
+    mockBackend.flush();
+
+    expect(ctrl.stocks).toEqual([{ticker: 'GOOG'}, {ticker: 'AMZN'}]);
+  });
+});

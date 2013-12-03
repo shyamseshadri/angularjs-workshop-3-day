d38e2c930d74afa64c40ec73c4f71b6900602448
Shyam Seshadri <shyamseshadri@gmail.com>
2013-12-03 23:27:43 +0530

    Session 4 - Step 5: Add e2e test for registration

--- test/e2e/e2eSpec.js
+++ test/e2e/e2eSpec.js
@@ -10,6 +10,26 @@ describe('Stock home page', function() {
     });
   });
 
+  it('should allow registering and clicking on all', function() {
+    browser.get('/');
+
+    element(by.css('.register-link')).click();
+
+    var rand = Math.ceil(Math.random() * 10000);
+    element(by.model('signupCtrl.username')).sendKeys('shyam' + rand);
+    element(by.model('signupCtrl.password')).sendKeys('test');
+
+    element(by.css('.btn-success')).click();
+
+    browser.debugger();
+    element(by.css('.btn-success.all')).click();
+
+    var all =
+      browser.findElements(by.repeater('stock in myStocksCtrl.stocks'));
+    all.then(function(arr) {
+      expect(arr.length).toEqual(11);
+    });
+  }, 20000);
 });
 
 

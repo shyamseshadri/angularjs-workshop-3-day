13ac025f07935020b819a69c140166fcfc2b9d58
Shyam Seshadri <shyamseshadri@gmail.com>
2013-12-03 23:15:03 +0530

    Session 4 - Step 1: Simple test for AlertService

--- test/spec/specs.js
+++ test/spec/specs.js
@@ -2,12 +2,20 @@
 
 describe('Service: AlertService', function () {
 
+  // load the controller's module
   beforeEach(module('stockMarketApp'));
 
+  var alertService;
   beforeEach(inject(function (AlertService) {
+    alertService = AlertService;
   }));
 
   it('should display error messages', function () {
-
+    expect(alertService.get()).toBeUndefined();
+    alertService.set('This is a test');
+    expect(alertService.get()).toEqual('This is a test');
+    alertService.clear();
+    expect(alertService.get()).toBeNull();
   });
+
 });
